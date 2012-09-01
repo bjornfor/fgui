21acd279388b4ddac20130da78bb02a44079f37f
Bjørn Forsman <bjorn.forsman@gmail.com>
2012-09-01 20:23:14 +0200

    Make fgui_widget_init() return a status code
    Initialization can fail if the list of children in the parent widget
    gets full.

--- src/fgui_widget.c
+++ src/fgui_widget.c
@@ -25,17 +25,20 @@ static int null_event_handler(struct fgui_widget *widget, struct fgui_event *eve
 	return 1;
 }
 
-void fgui_widget_init(struct fgui_widget *widget, struct fgui_widget *parent)
+int fgui_widget_init(struct fgui_widget *widget, struct fgui_widget *parent)
 {
+	int ret = 0;
+
 	memset(widget, 0, sizeof *widget);
 	widget->draw = null_draw;
 	widget->event_handler = null_event_handler;
 	widget->parent = parent;
 	if (parent) {
-		fgui_widget_add_child(parent, widget);
+		ret = fgui_widget_add_child(parent, widget);
 	}
 	widget->is_enabled = true;
 	widget->focus_policy = FGUI_TAB_FOCUS;
+	return ret;
 }
 
 void fgui_widget_set_draw(struct fgui_widget *widget, void (*draw)(struct fgui_widget *widget))
