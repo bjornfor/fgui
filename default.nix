b7ae17e63f099e60c296143066c6d9c3bd5864c2
Bjørn Forsman <bjorn.forsman@gmail.com>
2022-07-15 13:49:08 +0200

    default.nix: remove unused inputs: pkg-config, pythonFull

--- default.nix
+++ default.nix
@@ -7,10 +7,8 @@ with pkgs;
     name = "fgui-dev";
     src = ./.;
     buildInputs = [
-      pkgconfig  # for getting compiler flags to YouCompleteMe
       cmake
       SDL
-      pythonFull
       graphicsmagick
     ];
   };
